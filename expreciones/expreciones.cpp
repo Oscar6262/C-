de0156630200d298e9ecad3da25905b33640c16f
Oscar Gallardo <oscarivangallardoroman@gmail.com>
2021-09-21 18:07:06 -0600

    first commit

---
+++ expreciones/expreciones.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a{} ;
+
+    cout << "Ingrese un numero a multiplicar por dos: " ;
+    cin >> a ; // asigna valor en "a"
+    cout<<"Resultado: " << a*2 ; // Multiplica la variable "a" dos veces
+    
+    return 0;
+}
+
+ 
