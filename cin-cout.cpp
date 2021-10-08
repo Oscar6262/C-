16c16c080b2197cd3c3c1ce93df929b3f495e65b
Oscar6262 <90287005+Oscar6262@users.noreply.github.com>
2021-10-07 18:59:15 -0600

    inicio

---
+++ cin-cout.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Introduzca un numero:";
+    int x{};
+    cin>>x;
+    cout<<"Ingresaste " << x << '\n';
+    return 0;
+}
---
+++ ejercicio-expresion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a{} ;
+
+    cout << "Ingrese un numero a multiplicar por dos: " ;
+    cin >> a ; // asigna valor en "a"
+    cout<<"Resultado: " << a*2 ; // Multiplica la variable "a" dos veces
+    return 0;
+}
